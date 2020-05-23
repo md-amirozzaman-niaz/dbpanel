a8f779849f47f474c39044e5edeb8f34ab88a18d
niaz <md.amirozzaman@gmail>
2020-05-24 01:34:22 +0600

    go to page is added

--- src/resources/assets/js/app.js
+++ src/resources/assets/js/app.js
@@ -19,6 +19,7 @@ function setPagination(pageNo,total){
                 ulOfPagination.innerHTML += '<li class="page-item"><a class="page-link" >...</a></li>';
                 ulOfPagination.innerHTML += '<li class="page-item"><a class="page-link" onclick="getData('+total+')">'+total+'</a></li>';
             }
+            ulOfPagination.innerHTML += '<li class="page-item"><div class="d-flex"><input type="number" id="pageNumber" class="form-control brr-0" value=""><div class="btn brl-0" onclick="getDataFromGoTo()">Go</div></div></li>';
         }else{
 
             for(var i=1;i < total+1;i++){
@@ -28,6 +29,7 @@ function setPagination(pageNo,total){
         }
     }  
 }
+
 window.getData = function(pageNo=1){
     let url= document.getElementById('uri').value+"?"+document.getElementById('query').value+"&per_page="+document.getElementById('per_page').value+"&page="+pageNo;
     let dataDom = document.getElementById('data');
@@ -234,6 +236,10 @@ window.checkMethod =function(){
     }else{
         window.other();
     }
+}
+window.getDataFromGoTo=function(){
+    let p =document.getElementById('pageNumber').value;
+    window.getData(p);
 }
 window.viewInfo=function(){
     event.stopPropagation();
